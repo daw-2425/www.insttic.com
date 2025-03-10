06d6845e53ce628c749cfb08d7348dbcc183e895
ram-boss <ramboss914.com>
2025-03-10 17:41:05 +0100

    Actualizando

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -9,12 +9,9 @@ $dsn = "mysql:host=$servidor;dbname=$BaseDatos";
 $conexion = new PDO ("$dsn","$usuario","$passwd");
 $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-<<<<<<< HEAD
 // if($conexion){
 //     echo "conexion exitosa";
 // }
-=======
->>>>>>> 9fa87639fa8fabad038dae4f53f16bb0f550c2ba
 
 
 ?>
